2cd8f6511fa2dac225ba0ba601911e81a67b4bcc
sh <steamacc2013@mail.ru>
2020-07-07 20:50:55 +0300

    test items added

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 
 @pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
-def test_guest_should_see_login_link(browser, url):
+def test_button(browser, url):
     link = url
     browser.get(link)
     button = len(browser.find_elements_by_class_name("btn.btn-lg"))

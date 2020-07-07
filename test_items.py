c5942512f040c564426bc3e9959549f04d21f654
sh <steamacc2013@mail.ru>
2020-07-07 20:36:22 +0300

    test items added

---
+++ test_items.py
@@ -0,0 +1,12 @@
+import pytest
+import time
+from selenium import webdriver
+
+
+@pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+def test_guest_should_see_login_link(browser, url):
+    link = url
+    browser.get(link)
+    button = len(browser.find_elements_by_class_name("btn.btn-lg"))
+    assert button > 0, "There is no button"
+
